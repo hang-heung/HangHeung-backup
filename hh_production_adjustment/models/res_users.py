43725678407b64573c1fc18b381999fc42a38cf7
hang-heung <hang-heung@users.noreply.github.com>
2026-05-27 15:44:25 +0000

    feat: add separate Production Adjustment User group
    - New group group_hh_production_adj_user (Production Adjustment User)
    - Separate from Production Order User so access can be granted independently
    - Production Adjustment menu restricted to new group
    - Boolean toggle on user form (next to Production Order User)
    - zh_TW translations added for new group strings

--- hh_production_adjustment/models/res_users.py
+++ hh_production_adjustment/models/res_users.py
@@ -11,6 +11,13 @@ class ResUsers(models.Model):
         store=False,
         help="If enabled, this user can create and validate Production Orders.",
     )
+    production_adj_user = fields.Boolean(
+        string='Production Adjustment User',
+        compute='_compute_production_adj_user',
+        inverse='_inverse_production_adj_user',
+        store=False,
+        help="If enabled, this user can create and validate Production Adjustments.",
+    )
 
     def _compute_production_order_user(self):
         group = self.env.ref('hh_production_adjustment.group_hh_production_user')
@@ -21,6 +28,19 @@ class ResUsers(models.Model):
         group = self.env.ref('hh_production_adjustment.group_hh_production_user')
         for user in self:
             if user.production_order_user:
+                user.sudo().groups_id = [(4, group.id)]
+            else:
+                user.sudo().groups_id = [(3, group.id)]
+
+    def _compute_production_adj_user(self):
+        group = self.env.ref('hh_production_adjustment.group_hh_production_adj_user')
+        for user in self:
+            user.production_adj_user = group in user.groups_id
+
+    def _inverse_production_adj_user(self):
+        group = self.env.ref('hh_production_adjustment.group_hh_production_adj_user')
+        for user in self:
+            if user.production_adj_user:
                 user.sudo().groups_id = [(4, group.id)]
             else:
                 user.sudo().groups_id = [(3, group.id)]
